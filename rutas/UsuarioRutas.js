5f9bdb7519e4830531177511a45d9bd218e92f54
Andrés Felipe Méndez <andresmendez1220@gmail.com>
2022-10-29 13:12:14 -0500

    Creacion UsuarioRutas

---
+++ rutas/UsuarioRutas.js
@@ -0,0 +1,11 @@
+const ProductoOperaciones = require("../operaciones/ProductoOperaciones");
+const UsuarioOperaciones = require("../operaciones/UsuarioOperaciones");
+const router = require("express").Router();
+
+router.get("/", UsuarioOperaciones.consultarUsuarios);
+router.get("/:id", UsuarioOperaciones.consultarUsuario);
+router.post("/", UsuarioOperaciones.crearUsuario);
+router.put("/:id", UsuarioOperaciones.modificarUsuario);
+router.delete("/:id", UsuarioOperaciones.eliminarUsuario);
+
+module.exports = router;
