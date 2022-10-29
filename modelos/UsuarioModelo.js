97e520a0605e1bf9ee59390f228f16172e23aa33
Andrés Felipe Méndez <andresmendez1220@gmail.com>
2022-10-29 14:16:29 -0500

    Correccion de errores en UsuarioModelo

--- modelos/UsuarioModelo.js
+++ modelos/UsuarioModelo.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const usuarioSchema = mongoose.Schema({
-    nombres: { type: String, maxLength: 40, required: true },
-    apellidos: { type: String, maxLength: 40, required: true},
-    direccion: { type: String, maxLength: 40, required: true},
+    nombres: { type: String, maxLength: 80, required: true },
+    apellidos: { type: String, maxLength: 80, required: true},
+    direccion: { type: String, maxLength: 200, required: true},
     telefono: { type: String, maxLength: 20, required: true},
-    correo: { type: String, maxLength: 20, required: true}
+    correo: { type: String, maxLength: 100, required: true}
 });
 
 module.exports = mongoose.model("usuarios", usuarioSchema);
