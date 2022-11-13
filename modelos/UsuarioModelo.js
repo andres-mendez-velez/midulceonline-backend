27a0e2c3416f6f5a24f58bf5dd393f25616759cc
andres-mendez-velez <andresmendez1220@gmail.com>
2022-11-13 16:07:27 -0500

    Cambios UsuarioModelo

--- modelos/UsuarioModelo.js
+++ modelos/UsuarioModelo.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const usuarioSchema = mongoose.Schema({
-    nombres: { type: String, maxLength: 80, required: true },
-    apellidos: { type: String, maxLength: 80, required: true },
-    documento: { type: String, maxLength: 15, required: true, unique: true },
+    nombres: { type: String, required: true },
+    apellidos: { type: String, required: true },
+    documento: { type: String, required: true, unique: true },
     direccion: { type: String, maxLength: 200, required: true },
-    telefono: { type: String, maxLength: 20, required: true },
-    correo: { type: String, maxLength: 100, required: true, unique: true },
+    telefono: { type: String, required: true },
+    correo: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     admin: { type: Boolean, required: true }
 });
