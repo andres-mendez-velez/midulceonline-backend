37455081055b6b2d9ff31815f6ba07f590ca6390
andres-mendez-velez <andresmendez1220@gmail.com>
2022-11-16 16:37:49 -0500

    Carrito Modelo

---
+++ modelos/CarritoModelo.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const carritoSchema = mongoose.Schema({
+    nombreUser: { type: String, required: true },
+    apellidoUser: { type: String, required: true },
+    documentoUser: { type: String, required: true, unique: true },
+    direccionUser: { type: String, required: true },
+    telefonoUser: { type: String, required: true },
+    correo: { type: String, required: true, unique: true },
+    nombreProd: { type: String, required: true, unique: true },
+    cantidadPedida: { type: Number, required: true },
+    precioTotal: { type: Number, required: true },
+    estadoCompra: { type: String, required: true }
+});
+
+module.exports = mongoose.model("compras", carritoSchema);
